703629768357fd5f3c07bf79f81c9297dfb6a772
MinecraftFom <MinecraftFom477@gmail.com>
2026-02-19 16:36:43 +0800

    The main file of the plugin

---
+++ src/main/java/com/fomdev/translation/init/Translation.java
@@ -0,0 +1,34 @@
+package com.fomdev.translation.init;
+
+import com.fomdev.sasm.api.PluginClassUtil;
+import com.fomdev.translation.api.LangUtil;
+import com.fomdev.translation.commmand.CommandLanguage;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerLoadEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Translation extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        assert getCommand("language") != null;
+        getCommand("language").setExecutor(new CommandLanguage.LanguageCommand());
+        getCommand("language").setTabCompleter(new CommandLanguage.LanguageCompletion());
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onServerStart(ServerLoadEvent event) {
+        try {
+            PluginClassUtil.rescanCache();
+            LangUtil.syncDictionaryFromPackages();
+        } catch (Exception ignored) {
+
+        }
+    }
+}
